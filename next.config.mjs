8187e3a342cea8f3808ac09385a1afe8de109a1a
michele miranda <m.miranda@iconsulting.biz>
2025-07-28 14:47:03 +0200

    Fix: Disabilita export statico

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Aggiungi questa riga per l'esportazione statica
-  output: 'export',
-
-  // Le tue configurazioni esistenti vanno benissimo
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    // Questa opzione è necessaria per l'esportazione statica
-    unoptimized: true,
   },
 }
 
