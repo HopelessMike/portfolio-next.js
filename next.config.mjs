2e44be58e106fe40073ed31deebc3abdfa043d77
michele miranda <m.miranda@iconsulting.biz>
2025-08-26 23:26:08 +0200

    fix microfrontend.json v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
+// next.config.mjs (PORTFOLIO)
 import { withMicrofrontends } from '@vercel/microfrontends/next/config';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default withMicrofrontends(nextConfig);
